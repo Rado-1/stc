e2b3b0a041e727f69a4990adeea19e68baab8b58
Radovan Cervenka <radovan.cervenka@gmail.com>
2022-12-13 19:57:57 +0100

    Settings updated, levels added

--- scenes/title.gd
+++ scenes/title.gd
@@ -15,26 +15,22 @@ func _ready():
 func _set_achieved_level():
 	if  Game.current_level > 0:
 		$AchievedLevelLabel.text = "Achieved level: %d" % Game.current_level
-		$ResetLevelButton.visible = true
 	else:
 		$AchievedLevelLabel.text = ""
-		$ResetLevelButton.visible = false
 
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("ui_cancel") && !_is_settings_open:
-		get_tree().quit()
+	if Input.is_action_just_pressed("ui_cancel"):
+		if _is_settings_open:
+			_on_OkButton_pressed()
+		else:
+			get_tree().quit()
+	elif Input.is_action_just_pressed("ui_accept") && _is_settings_open:
+		_on_OkButton_pressed()
 	elif event.is_pressed() && !_is_settings_open:
 		_is_settings_open = true # hack to filter multiple taps on Android
 		Audio.button_click()
 		Game.goto_next_level()
-
-
-func _on_ResetLevelButton_pressed():
-	Audio.button_click()
-	Game.current_level = 0
-	Game.save_current_level()
-	_set_achieved_level()
 
 
 func _on_ExitButton_pressed():
@@ -60,6 +56,13 @@ func _on_FxSlider_value_changed(value):
 	Audio.set_volumes(-1, value)
 	Audio.button_click()
 	$SettingsDialog/FxSprite.animation = "off" if value < 0.01 else "on"
+
+
+func _on_ResetLevelButton_pressed():
+	Audio.button_click()
+	Game.current_level = 0
+	Game.save_current_level()
+	_set_achieved_level()
 
 
 func _on_OkButton_pressed():
