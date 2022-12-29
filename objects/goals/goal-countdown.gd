0478083dae6047195e95738d4a5f7768a29533b6
Radovan Cervenka <radovan.cervenka@gmail.com>
2022-12-29 15:56:47 +0100

    AntialiasedLine2D removed

--- objects/goals/goal-countdown.gd
+++ objects/goals/goal-countdown.gd
@@ -2,5 +2,18 @@ class_name GoalCountdown
 extends Node2D
 
 
+const ANGLE_TOP = -TAU / 4.0
+const ANGLE_FULL = TAU * 1.1
+
+
+var _value: float
+
+
 func set_progress(value: float):
-	$Meter.angle_degrees = value * 360.0
+	_value = value * TAU + ANGLE_TOP
+	update()
+
+
+func _draw():
+	draw_arc(Vector2.ZERO, 20.0, 0.0, ANGLE_FULL, 32, Color.beige, 10.0, true)
+	draw_arc(Vector2.ZERO, 20.0, ANGLE_TOP, _value, 32, Color.crimson, 8.0, true)
